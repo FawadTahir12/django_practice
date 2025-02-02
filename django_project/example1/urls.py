7d34594714381b000dcefe519db3ab8c6def0225
ftahir123 <fawad.tahir@alpharages.com>
2025-02-02 22:25:59 +0500

    Add JWT authentication

--- django_project/example1/urls.py
+++ django_project/example1/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.createUserView.as_view(), name="user_view_create"),
     path("<str:email>/", views.updateUserView.as_view(), name="user_view_update"),
+    path("login", views.LoginView.as_view(), name="login"),
 ]
